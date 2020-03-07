fddfd01bf3bfab930a5ff1c42606ab3dd90849e2
juchani <victorfrancojuchani@gmail.com>
2020-03-07 07:41:55 -0400

    estructuracion

--- servo/servo.py
+++
@@ -1,7 +0,0 @@
-# ejemplo micropython 
-# te interesa mas sobre micropython? 
-# unete a nuestra comunidad https://www.facebook.com/ARDUINODAY19SC/
-# EL DOMINIO DE  ESTE CODIGO ES PUBLICO
-import machine                                                                                                                                    
-servo=machine.PWM(machine.Pin(12),freq=50)                                                                                                        
-servo.duty(10)      
