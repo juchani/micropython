6794957c65a9c090e6f2c023f873258e4e5a0c5f
juchani <victorfrancojuchani@gmail.com>
2020-03-19 10:50:29 -0400

    19-03

--- micropython/m.py
+++
@@ -1,4 +0,0 @@
-from time import sleep as t 
-while 1:
-     print("1")
-     t(1)
--- micropython/secuencia led (interno)/main.py
+++ micropython/secuencia led (interno)/main.py
@@ -1,7 +1,6 @@
 from pyb import LED as l 
 from time import sleep as t
 led=[l(1),l(2),l(3),l(4)] 
-
 while 1:
     for n in led:
         n.toggle()
