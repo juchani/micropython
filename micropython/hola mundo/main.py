8454e9f9a285acaad6d0ceda7fbeef73da644258
juchani <victorfrancojuchani@gmail.com>
2020-03-19 12:38:08 -0400

    19-03

--- micropython/hola mundo/main.py
+++ micropython/hola mundo/main.py
@@ -1,15 +1,7 @@
 from pyb import LED as l 
 from time import sleep as t
-from pyb import Accel
-accel = Accel().x
-led_i=[l(1),l(2),l(3),l(4)]
-def x():
-    x=int((int(accel())+21)/14)
-    print(x)
-    return x
+led=l(1)
 
 while 1:
-    for h in led_i:
-        h.off()
-    led_i[x()].on()
-    t(0.2)
+    led.toggle()
+    t(1)
